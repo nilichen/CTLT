3df0c178e527add82920f8578e05852c3e3843e6
Lichen Ni <lichenni1990@gmail.com>
2016-08-16 10:10:26 -0700

    added shell scripts

---
+++ reports/daily.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd /Users/katrinani/Documents/CTLT/analysis/reports
+python daily_reports.py
---
+++ reports/weekly.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /Users/katrinani/Documents/CTLT/analysis/reports
+python weekly_reports.py
+
